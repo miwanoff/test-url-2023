6f0e6ff3793bd07fcee527400c4eb52839500a7a
miwanoff <marina.iva.noffa@gmail.com>
2023-10-15 15:19:20 +0300

    Add $valid_form

--- form_process.php
+++ form_process.php
@@ -1,12 +1,26 @@
 <?php
-if (isset($_POST['submit']))
-{
-  echo "Your First Name is:" .$_POST["FName"] . "<br/>";
-  echo "Your Last Name is:" .$_POST["LName"] . "<br/>";
-  echo "Your City is:" .$_POST["City"]."<br/>";
-  echo "Your State is:" .$_POST["State"]."<br/>";
-  echo "<br/>"; 
-  echo "Your Message is:" .$_POST["Message"];   
+if (isset($_POST['submit'])) {
+    $valid_form = true;
+    if ($_POST['FName'] == "") {
+        echo "Введіть своє ім'я! ";
+        $valid_form = false;
+    } else {
+        $name = $_POST['FName'];
+    }
+    if ($_POST['LName'] == "") {
+        echo "Введіть ім'я коритстувача! ";
+        $valid_form = false;
+    } else {
+        $username = $_POST['LName'];
+    }
+    if ($valid_form === true) {
+        echo "Your First Name is:" . $_POST["FName"] . "<br/>";
+        echo "Your Last Name is:" . $_POST["LName"] . "<br/>";
+    }
+    // echo "Your City is:" . $_POST["City"] . "<br/>";
+    // echo "Your State is:" . $_POST["State"] . "<br/>";
+    // echo "<br/>";
+    // echo "Your Message is:" . $_POST["Message"];
 }
 ?>
 
@@ -19,14 +33,15 @@ if (isset($_POST['submit']))
 </head>
 
 <body>
-    <form action="<?= $_SERVER['PHP_SELF']?>" method="POST">
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
         First Name:<br />
         <input type="text" name="FName" /><br />
         Last Name:<br /> <input type="text" name="LName" /><br />
-        City:<br /> <input type="text" name="City" /><br />
+        <!-- City:<br /> <input type="text" name="City" /><br />
         State:<br /> <input type="text" name="State" /><br />
         Message:<br /> <textarea name="Message" cols="30" rows="5">
-      </textarea><br /> <input type="submit" name="submit" value="Submit Data" />
+      </textarea><br /> -->
+        <input type="submit" name="submit" value="Submit Data" />
     </form>
 </body>
 
