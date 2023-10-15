93618900f68340073f42947f82923ed681d6914e
miwanoff <marina.iva.noffa@gmail.com>
2023-10-15 15:11:09 +0300

    Add isset

---
+++ form_process.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['submit']))
+{
+  echo "Your First Name is:" .$_POST["FName"] . "<br/>";
+  echo "Your Last Name is:" .$_POST["LName"] . "<br/>";
+  echo "Your City is:" .$_POST["City"]."<br/>";
+  echo "Your State is:" .$_POST["State"]."<br/>";
+  echo "<br/>"; 
+  echo "Your Message is:" .$_POST["Message"];   
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>A Web Page</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <form action="<?= $_SERVER['PHP_SELF']?>" method="POST">
+        First Name:<br />
+        <input type="text" name="FName" /><br />
+        Last Name:<br /> <input type="text" name="LName" /><br />
+        City:<br /> <input type="text" name="City" /><br />
+        State:<br /> <input type="text" name="State" /><br />
+        Message:<br /> <textarea name="Message" cols="30" rows="5">
+      </textarea><br /> <input type="submit" name="submit" value="Submit Data" />
+    </form>
+</body>
+
+</html>
