49c91d2d5990e3d685a377affa5633eb314dbe9f
miwanoff <marina.iva.noffa@gmail.com>
2023-10-15 15:44:47 +0300

    Add select and radio

--- form_process.php
+++ form_process.php
@@ -17,6 +17,24 @@ if (isset($_POST['submit'])) {
         echo "Your First Name is:" . $_POST["FName"] . "<br/>";
         echo "Your Last Name is:" . $_POST["LName"] . "<br/>";
     }
+
+    $result_answer = $_POST['answer'];
+    if ($_POST['answer'] === "yes") {
+        $result_answer = "Да!";
+    }
+
+    if ($_POST['answer'] === "no") {
+        $result_answer = "Нет!";
+    }
+
+    echo "<p>Вы ответили: <b>" . $result_answer . "</b></p>";
+
+    if (isset($_POST['courses'])) {
+        $courses = $_POST['courses'];
+        foreach ($courses as $item) {
+            echo "$item<br>";
+        }
+    }
     // echo "Your City is:" . $_POST["City"] . "<br/>";
     // echo "Your State is:" . $_POST["State"] . "<br/>";
     // echo "<br/>";
@@ -37,6 +55,14 @@ if (isset($_POST['submit'])) {
         First Name:<br />
         <input type="text" name="FName" /><br />
         Last Name:<br /> <input type="text" name="LName" /><br />
+        <input type="radio" name="answer" value="yes" checked>Да<br />
+        <input type="radio" name="answer" value="no">Нет<br />
+        <select name="courses[]" size="2" multiple="multiple">
+            <option value="ASP.NET">ASP.NET</option>
+            <option value="PHP">PHP</option>
+            <option value="Ruby">RUBY</option>
+            <option value="Python">Python</option>
+        </select><br>
         <!-- City:<br /> <input type="text" name="City" /><br />
         State:<br /> <input type="text" name="State" /><br />
         Message:<br /> <textarea name="Message" cols="30" rows="5">
