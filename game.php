75211b0f8b7c50a9b44c73b381647018aaf1bcd1
miwanoff <marina.iva.noffa@gmail.com>
2023-10-21 14:59:43 +0300

    Add hidden field n

--- game.php
+++ game.php
@@ -1,36 +1,41 @@
 <?php
 
-$n = 5; //Задумане число
+$n = rand(1, 8); //Задумане число
 $count = 0; // Кількість спроб
 $text = ""; // Текст підсказки
 $nameErr = ""; // Повідомлення про помилку
+//echo $n."\n";
+if (isset($_REQUEST['Submit'])) { // Якщо натиснута кнопка 'Submit'
+    $n = $_REQUEST['n'];
+    echo $n . "\n";
+    $count = $_REQUEST['count'] + 1; // Збільшуємо лічильник на 1
 
-if (isset($_POST['Submit'])) { // Якщо натиснута кнопка 'Submit'
-  $count = $_POST['hidden'] + 1;// Збільшуємо лічильник на 1
+    echo $count;
 
-  if (empty($_POST["my_number"])) { // Якщо нічого не ввели
-    $nameErr = "Число обов'язкове для введення!";
-  } else {
-    $my_number = trim($_POST["my_number"]);// Видаляємо зайві прогалини
-
-    // перевірка, чи міститься лише число
-    if (!preg_match("/^[1-8]$/", $my_number)) {
-      $nameErr = "Дозволяється лише число від 1 до 8!";
-    }
-  }
-  if ($nameErr === "") { // Якщо не було помилки
-    if ($my_number > $n)
-      $text = "Занадто багато!";
-    elseif ($my_number < $n) {
-      $text = "Замало!";
+    if (empty($_REQUEST["my_number"])) { // Якщо нічого не ввели
+        $nameErr = "Число обов'язкове для введення!";
     } else {
-      $text = "Точно! Вгадано з $count спроби!<br/>";
+        $my_number = trim($_REQUEST["my_number"]); // Видаляємо зайві прогалини
+
+        // перевірка, чи міститься лише число
+        if (!preg_match("/^[1-8]$/", $my_number)) {
+            $nameErr = "Дозволяється лише число від 1 до 8!";
+        }
     }
-  }
+    if ($nameErr === "") { // Якщо не було помилки
+        if ($my_number > $n) {
+            $text = "Занадто багато!";
+        } elseif ($my_number < $n) {
+            $text = "Замало!";
+        } else {
+            $text = "Точно! Вгадано з $count спроби!<br/>";
+        }
+    }
 }?>
 <p>Вгадай число от 1 до 8:</p>
-<form action="<?= $_SERVER['PHP_SELF']?>" name="myform" method="POST">
-    <input type="text" name="my_number" size="5"><?= $text ?><?= $nameErr ?><br />
-    <input type="hidden" name="hidden" size="50" value="<?= $count ?>">
+<form action="<?=$_SERVER['PHP_SELF']?>" name="myform" method="POST">
+    <input type="text" name="my_number" size="5"><?=$text?><?=$nameErr?><br />
+    <input type="hidden" name="count" size="50" value="<?=$count?>">
+    <input type="hidden" name="n" size="50" value="<?=$n?>">
     <input name="Submit" type="submit" value="Відправити"><br />
 </form>
